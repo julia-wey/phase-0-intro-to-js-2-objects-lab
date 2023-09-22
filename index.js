80f0f1ca39a3b41af9a2cdf561e6f30e42c189df
Julia Wey-Burgoyne <juliawey@gmail.com>
2023-09-21 22:10:09 -0400

    Adds Objects Lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const employee = {}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const employeeTwo = {... employee }
+    employeeTwo[key] = value
+    return employeeTwo
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value
+    return employee
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const employeeThree = {...employee}
+    delete employeeThree[key]
+    return employeeThree
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key]
+    return employee
+}
